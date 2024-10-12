a61d3186ed4c7785daf2113589be7f45d63caa55
muyiacc <115904774+muyiacc@users.noreply.github.com>
2024-10-12 11:19:50 +0800

    搭建基础框架

---
+++ src/views/button.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="mb-4">
+        <el-button>Default</el-button>
+        <el-button type="primary">Primary</el-button>
+        <el-button type="success">Success</el-button>
+        <el-button type="info">Info</el-button>
+        <el-button type="warning">Warning</el-button>
+        <el-button type="danger" disabled>Danger</el-button>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+div {
+    margin: .3125rem;
+    padding: .3125rem;
+}
+</style>
